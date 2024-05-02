323e7d9da06fbbd11fe097122248e99d3338524c
Daniel Cho <danielcho@daniels-mbp-475.devices.brown.edu>
2024-05-02 16:17:16 -0400

    updated training agents and waterfall

--- rl/training_agents.py
+++ rl/training_agents.py
@@ -1,8 +1,9 @@
 
-from typing import Set, Dict
+from typing import Set, Dict, List
 
 import sys, os
 sys.path.append("..")
+import random
 
 from adx.agents import NDaysNCampaignsAgent
 from adx.tier1_ndays_ncampaign_agent import Tier1NDaysNCampaignsAgent
@@ -21,9 +22,10 @@ market_segments = [
     MarketSegment(("Female", "Old", "HighIncome"))
 ]
 
+
 class CriticalNDaysNCampaignsAgent(NDaysNCampaignsAgent):
 
-    def __init__(self, name = "Ciritical Agent"):
+    def __init__(self, name = "Critical Agent"):
         super().__init__()
         self.name = name
 
@@ -62,17 +64,351 @@ class CriticalNDaysNCampaignsAgent(NDaysNCampaignsAgent):
         return bundles
 
     def get_campaign_bids(self, campaigns_for_auction:  Set[Campaign]) -> Dict[Campaign, float]:
-        # TODO: fill this in 
         bids = {}
         for campaign in campaigns_for_auction:
             bids[campaign] = campaign.reach * 2
         return bids
 
+
+class RandomNDaysNCampaignsAgent(NDaysNCampaignsAgent):
+
+    def __init__(self, name = "Random Agent"):
+        super().__init__()
+        self.name = name
+        self.random = random
+
+    def on_new_game(self) -> None:
+        pass
+
+    def get_ad_bids(self) -> Set[BidBundle]:
+        bundles = set()
+        campaigns = self.get_active_campaigns().union(self.my_campaigns)
+        for campaign in campaigns:
+            bid_entries = set()
+            subsets = set()
+            for segment in market_segments:
+                if campaign.target_segment.issuperset(segment):
+                    subsets.add(segment)
+            n = len(subsets)
+            for segment in subsets:
+                auction_item = segment
+                bid_per_item = campaign.budget / (n * campaign.reach)
+                alpha = 1.0 + (self.random.random() - 0.5) / 3.0
+                bid_per_item *= alpha
+                bid = Bid(
+                    bidder=self,
+                    auction_item=auction_item,
+                    bid_per_item=bid_per_item,
+                    bid_limit=campaign.budget / n
+                )
+                bid_entries.add(bid)
+            limit = campaign.budget
+            bundle = BidBundle(
+                campaign_id=campaign.uid,
+                limit=limit,
+                bid_entries=bid_entries
+            )
+            bundles.add(bundle)
+
+        return bundles
+
+    def get_campaign_bids(self, campaigns_for_auction:  Set[Campaign]) -> Dict[Campaign, float]:
+        bids = {}
+        for campaign in campaigns_for_auction:
+            bids[campaign] = campaign.reach * self.random.random()
+        return bids
+
+
+class BetterNDaysNCampaignsAgent(CriticalNDaysNCampaignsAgent):
+
+    def __init__(self, name = "Better Agent"):
+        super().__init__()
+        self.name = name
+
+        self.auctions = 0
+        self.auctions_won = 0
+
+        self.campaign_auction_history: List[Campaign] = []
+        
+    
+    def count_won_auctions(self, campaigns_for_auction:  Set[Campaign]):
+        
+        # count number of won auctions
+        self.auctions += len(self.campaign_auction_history)
+        my_campaigns = self.get_active_campaigns().union(self.my_campaigns)
+        for campaign in self.campaign_auction_history:
+            if campaign in my_campaigns:
+                self.auctions_won += 1
+
+        # store campaigns for next iteration
+        self.campaign_auction_history = campaigns_for_auction
+
+    def get_campaign_bids(self, campaigns_for_auction:  Set[Campaign]) -> Dict[Campaign, float]:
+        self.count_won_auctions(campaigns_for_auction)
+        bids = {}
+        for campaign in campaigns_for_auction:
+            # bids[campaign] = campaign.reach * 0.125
+            bids[campaign] = campaign.reach * 0.1
+        return bids
+    
+
+class CampaignNDaysNCampaignsAgent(BetterNDaysNCampaignsAgent):
+
+    def __init__(self, name = "Campaign Agent"):
+        super().__init__()
+        self.name = name
+
+        self.random = random
+        self.campaign_history: Dict[MarketSegment, float] = {}
+
+    def on_new_game(self) -> None:
+        # self.campaign_history = {} # reset history... does not improve performance...
+        pass
+
+    def get_campaign_bids(self, campaigns_for_auction:  Set[Campaign]) -> Dict[Campaign, float]:
+
+        # parameters
+        delta = 0.1
+        min_alpha = 0.1
+        reward_multiplier = 0.25
+
+        # update auction prices for each campaign
+        for campaign in self.campaign_auction_history:
+            my_campaigns = self.get_active_campaigns().union(self.my_campaigns)
+            # if self.random.random() >= 0.5:
+            if campaign not in my_campaigns:
+                alpha = self.campaign_history[campaign.target_segment] - delta
+                self.campaign_history[campaign.target_segment] = max(alpha, min_alpha)
+            else:
+                self.campaign_history[campaign.target_segment] += delta * reward_multiplier
+
+        # counting for stats
+        self.count_won_auctions(campaigns_for_auction)
+
+        # create bids
+        bids = {}
+        for campaign in campaigns_for_auction:
+            if campaign.target_segment not in self.campaign_history:
+                self.campaign_history[campaign.target_segment] = 0.25
+            bids[campaign] = campaign.reach * self.campaign_history[campaign.target_segment]
+
+        # return campaign bids
+        return bids
+    
+
+class RandCampNDaysNCampaignsAgent(BetterNDaysNCampaignsAgent):
+
+    def __init__(self, name = "RandCamp Agent"):
+        super().__init__()
+        self.name = name
+
+        self.random = random
+        self.campaign_history: Dict[MarketSegment, float] = {}
+
+    def on_new_game(self) -> None:
+        # self.campaign_history = {} # reset history... does not improve performance...
+        pass
+
+    def get_campaign_bids(self, campaigns_for_auction:  Set[Campaign]) -> Dict[Campaign, float]:
+
+        # parameters
+        delta = 0.1
+        min_alpha = 0.1
+        reward_multiplier = 0.25
+
+        # update auction prices for each campaign
+        for campaign in self.campaign_auction_history:
+            my_campaigns = self.get_active_campaigns().union(self.my_campaigns)
+            if campaign not in my_campaigns:
+                alpha = self.campaign_history[campaign.target_segment] - delta
+                alpha *= 1. + (self.random.random() - 0.45) / 3 # randomness
+                self.campaign_history[campaign.target_segment] = max(alpha, min_alpha)
+            else:
+                self.campaign_history[campaign.target_segment] += delta * reward_multiplier
+
+        # counting for stats
+        self.count_won_auctions(campaigns_for_auction)
+
+        # create bids
+        bids = {}
+        for campaign in campaigns_for_auction:
+            if campaign.target_segment not in self.campaign_history:
+                self.campaign_history[campaign.target_segment] = 0.25
+            bids[campaign] = campaign.reach * self.campaign_history[campaign.target_segment]
+            # bids[campaign] *= 1. + (self.random.random() - 0.45) / 3
+
+        # return campaign bids
+        return bids
+    
+
+class SmartNDaysNCampaignsAgent(BetterNDaysNCampaignsAgent):
+
+    def __init__(self, name = "Smart Agent", score_multiplier: float = 1.):
+        super().__init__()
+        self.name = name
+
+        self.score_multiplier = score_multiplier
+        self.random = random
+        self.campaign_history: Dict[MarketSegment, float] = {}
+
+    def on_new_game(self) -> None:
+        # self.campaign_history = {} # reset history... does not improve performance...
+        pass
+
+    def get_campaign_bids(self, campaigns_for_auction:  Set[Campaign]) -> Dict[Campaign, float]:
+
+        # parameters
+        delta = 0.1
+        min_alpha = 0.1
+        reward_multiplier = 0.25
+
+        # take own campaigns into account
+        my_campaigns = self.get_active_campaigns().union(self.my_campaigns)
+
+        # remove inactive campaigns
+        reduced_campaigns: Set[Campaign] = set()
+        for campaign in my_campaigns:
+            if campaign.end_day < self.current_day:
+                continue
+            reduced_campaigns.add(campaign)
+
+        # update auction prices for each campaign
+        for campaign in self.campaign_auction_history:
+            if campaign not in my_campaigns:
+                alpha = self.campaign_history[campaign.target_segment] - delta
+
+                alpha *= 1. + (self.random.random() - 0.45) / 3 # randomness
+
+                if len(reduced_campaigns) > 0: 
+                    score = 0
+                    for reduced_campaign in reduced_campaigns:
+                        if reduced_campaign.target_segment.issubset(campaign.target_segment) or reduced_campaign.target_segment.issuperset(campaign.target_segment):
+                            score += 1
+                    score /= len(reduced_campaigns)
+                    alpha *= 1. - score * self.score_multiplier
+                    # alpha *= 1. + score * 0.1
+                
+                self.campaign_history[campaign.target_segment] = max(alpha, min_alpha)
+            else:
+                self.campaign_history[campaign.target_segment] += delta * reward_multiplier
+
+        # counting for stats
+        self.count_won_auctions(campaigns_for_auction)
+
+        # create bids
+        bids = {}
+        for campaign in campaigns_for_auction:
+            if campaign.target_segment not in self.campaign_history:
+                self.campaign_history[campaign.target_segment] = 0.25
+            bids[campaign] = campaign.reach * self.campaign_history[campaign.target_segment]
+            # bids[campaign] *= 1. + (self.random.random() - 0.45) / 3
+
+        # return campaign bids
+        return bids
+    
+class LessRandNDaysNCampaignsAgent(BetterNDaysNCampaignsAgent):
+
+    def __init__(self, name = "LessRand Agent", score_multiplier: float = 1.):
+        super().__init__()
+        self.name = name
+
+        self.score_multiplier = score_multiplier
+        self.random = random
+        self.campaign_history: Dict[MarketSegment, float] = {}
+
+    def on_new_game(self) -> None:
+        # self.campaign_history = {} # reset history... does not improve performance...
+        pass
+
+    def get_campaign_bids(self, campaigns_for_auction:  Set[Campaign]) -> Dict[Campaign, float]:
+
+        # parameters
+        delta = 0.1
+        min_alpha = 0.1
+        reward_multiplier = 0.25
+
+        # take own campaigns into account
+        my_campaigns = self.get_active_campaigns().union(self.my_campaigns)
+
+        # remove inactive campaigns
+        reduced_campaigns: Set[Campaign] = set()
+        for campaign in my_campaigns:
+            if campaign.end_day < self.current_day:
+                continue
+            reduced_campaigns.add(campaign)
+
+        # update auction prices for each campaign
+        for campaign in self.campaign_auction_history:
+            if campaign not in my_campaigns:
+                alpha = self.campaign_history[campaign.target_segment] - delta
+
+                alpha *= 1. + (self.random.random() - 0.45) / 5 # randomness
+                # alpha *= 1. + (self.random.random() - 0.45) / 8 # randomness
+
+                # take into account the number of days...
+
+                if len(reduced_campaigns) > 0: 
+                    score = 0
+                    for reduced_campaign in reduced_campaigns:
+                        if reduced_campaign.target_segment.issubset(campaign.target_segment) or reduced_campaign.target_segment.issuperset(campaign.target_segment):
+                            score += 1
+                    score /= len(reduced_campaigns)
+                    alpha *= 1. - score * self.score_multiplier
+                self.campaign_history[campaign.target_segment] = max(alpha, min_alpha)
+            else:
+                self.campaign_history[campaign.target_segment] += delta * reward_multiplier
+
+        # counting for stats
+        self.count_won_auctions(campaigns_for_auction)
+
+        # create bids
+        bids = {}
+        for campaign in campaigns_for_auction:
+            if campaign.target_segment not in self.campaign_history:
+                self.campaign_history[campaign.target_segment] = 0.25
+            bids[campaign] = campaign.reach * self.campaign_history[campaign.target_segment]
+            # bids[campaign] *= 1. + (self.random.random() - 0.45) / 3
+
+        # return campaign bids
+        return bids
+
+
 if __name__ == "__main__":
     # Here's an opportunity to test offline against some TA agents. Just run this file to do so.
     # test_agents = [CriticalNDaysNCampaignsAgent()] + [Tier1NDaysNCampaignsAgent(name=f"Agent {i + 1}") for i in range(9)]
-    test_agents = [CriticalNDaysNCampaignsAgent()] + [CriticalNDaysNCampaignsAgent(name=f"Agent {i + 1}") for i in range(9)]
+    
+    # smart_agent = SmartNDaysNCampaignsAgent()
+    randcamp_agent = RandCampNDaysNCampaignsAgent()
+    campaign_agent = CampaignNDaysNCampaignsAgent()
+    better_agent = BetterNDaysNCampaignsAgent()
+    lessrand_agent = LessRandNDaysNCampaignsAgent(name="LessRand Agent", score_multiplier=-2)
+    
+    test_agents  = [SmartNDaysNCampaignsAgent(name=f"Smart Agent 1", score_multiplier=3), SmartNDaysNCampaignsAgent(name=f"Smart Agent 2", score_multiplier=-2), SmartNDaysNCampaignsAgent(name=f"Smart Agent 3", score_multiplier=-3)]
+    test_agents += [randcamp_agent, campaign_agent, better_agent, lessrand_agent]
+    test_agents += [RandomNDaysNCampaignsAgent(name=f"Rng Agent {i + 1}") for i in range(2)]
+    test_agents += [CriticalNDaysNCampaignsAgent(name=f"Crit Agent {i + 1}") for i in range(1)]
 
     # Don't change this. Adapt initialization to your environment
     simulator = AdXGameSimulator()
-    simulator.run_simulation(agents=test_agents, num_simulations=10)
+    simulator.run_simulation(agents=test_agents, num_simulations=2000)
+
+    # agent history debugging
+    def stats_print(agent: BetterNDaysNCampaignsAgent):
+        print(f"\n{agent.name} won {agent.auctions_won}/{agent.auctions} auctions | {round(float(agent.auctions_won) / float(agent.auctions) * 100, 3)}%\n")
+    
+    for agent in test_agents:
+        if hasattr(agent, 'auctions_won'):
+            stats_print(agent)
+        else:
+            print(f"{agent.name} no stats!")
+
+
+    # campaign agent won...
+    # smart agent 1 won...
+    # randcamp agent won...
+
+    # i want a concrete victor...
+    # little less randomness...
+
+    # mess with the randomness a little more...
+    # throw out the losing peoples...
--- waterfall_algorithm.py
+++ waterfall_algorithm.py
@@ -1,41 +1,98 @@
-from typing import Set
+from typing import Set, Dict, List, Tuple
 from adx.structures import Campaign, MarketSegment
 import numpy as np
 
-# This uses the waterfall algorithm to calculate optimal prices for a
-# campaign in a second price auction.
+# Given campaigns, calculates alllocation and price tables
+
+# TODO:
 
 
-# so far this only creates the allocation and price tables...
+    # how to get this to beat the normal agents...
+
+    # get a specific agent to beat out the critical bidding agents...
 
 
-def market_segment_to_key(segment: MarketSegment):
-    output = ""
-    for item in segment:
-        output += item[0]
-    return output
 
-def run(campaigns: Set[Campaign]):
 
-    # user_frequencies = {
-    #     MarketSegment(("Male", "Young", "LowIncome")): 1836,
-    #     MarketSegment(("Male", "Young", "HighIncome")): 517,
-    #     MarketSegment(("Male", "Old", "LowIncome")): 1795,
-    #     MarketSegment(("Male", "Old", "HighIncome")): 808,
-    #     MarketSegment(("Female", "Young", "LowIncome")): 1980,
-    #     MarketSegment(("Female", "Young", "HighIncome")): 256,
-    #     MarketSegment(("Female", "Old", "LowIncome")): 2401,
-    #     MarketSegment(("Female", "Old", "HighIncome")): 407
-    # }
+    # figure out how to train the rl better...
+    # get agents that are decently smart to train against...
 
+    # smartest agent without using rl...
+
+    # figure out different possible strategies for this...
+        # 2 max random agents...
+        # 2 critical bidding agents
+        # rest are very smart agents...
+
+
+
+
+
+    # get the rl to predict campaigns out there...
+        # if this proves to be too difficult of a problem, just run the WF or WE on what we do know and then let rl to shade shit higher or lower...
+
+        # simplify this problem by only caring about stuff...
+        # wait why cant we just use our knowledge to guess this stuff...
+
+        # no reason to have rl deal with this...
+
+
+
+    
+    # more history information neeeded... figure out if we won the auctions or not...
+    # if we did lose that stuff then inputs given as shtuff...
+
+    # tuning hyperparameters... higher learning rate because we don't have 1million training iterations...
+
+
+
+
+    # what's the difference between active campaigns and my campaigns...
+    # why tf does this not include the first campaign... sort these out by the day to filter stuff out...
+
+
+
+
+
+
+
+def run(day: int, campaigns: Set[Campaign]) -> Dict[Campaign, List[Tuple[MarketSegment, float, int]]]:
+
+    # frequencies for the day
     user_frequencies = {
-        MarketSegment(("Male",)): 8,
-        MarketSegment(("Female",)): 7
+        MarketSegment(("Male", "Young", "LowIncome")): 1836,
+        MarketSegment(("Male", "Young", "HighIncome")): 517,
+        MarketSegment(("Male", "Old", "LowIncome")): 1795,
+        MarketSegment(("Male", "Old", "HighIncome")): 808,
+        MarketSegment(("Female", "Young", "LowIncome")): 1980,
+        MarketSegment(("Female", "Young", "HighIncome")): 256,
+        MarketSegment(("Female", "Old", "LowIncome")): 2401,
+        MarketSegment(("Female", "Old", "HighIncome")): 407
     }
 
+    # debugging example
+    user_frequencies = {
+        MarketSegment(("Male", "Young", "LowIncome")): 10,
+        MarketSegment(("Male", "Young", "HighIncome")): 517,
+        MarketSegment(("Male", "Old", "LowIncome")): 1795,
+        MarketSegment(("Male", "Old", "HighIncome")): 808,
+        MarketSegment(("Female", "Young", "LowIncome")): 1980,
+        MarketSegment(("Female", "Young", "HighIncome")): 256,
+        MarketSegment(("Female", "Old", "LowIncome")): 2401,
+        MarketSegment(("Female", "Old", "HighIncome")): 407
+    }
+    # user_frequencies = {
+    #     MarketSegment(("Male",)): 8,
+    #     MarketSegment(("Female",)): 7
+    # }
 
-
-
+    # remove campaigns if their end date has passed
+    reduced_campaigns: Set[Campaign] = set()
+    for campaign in campaigns:
+        if campaign.end_day < day: continue
+        reduced_campaigns.add(campaign)
+    campaigns = reduced_campaigns
+    
     # create a tuple list of critical bids
     critical_bids = []
     for campaign in campaigns:
@@ -45,100 +102,47 @@ def run(campaigns: Set[Campaign]):
     # sort these campaigns from highest to lowest
     critical_bids.sort(key=lambda input: input[0], reverse=True)
 
+    # allocations 
+    allocation_table = {}
+    for i in range(len(critical_bids)):
+        critical_bid, campaign = critical_bids[i]
+
+        # find all sub-market segments
+        sub_market_segments = []
+        for segment in user_frequencies:
+            if segment.issuperset(campaign.target_segment):
+                sub_market_segments.append((0, segment))
+
+        # find second highest bid for each sub-market segment
+        for j in range(len(sub_market_segments)):
+            _, segment = sub_market_segments[j]
+            for k in range(i+1, len(critical_bids)):
+                second_critical_bid, second_campaign = critical_bids[k]
+                if segment.issubset(second_campaign.target_segment) or segment.issuperset(second_campaign.target_segment):
+                    sub_market_segments[j] = second_critical_bid, segment
+                    break
+
+        # sort sub market segments 
+        sub_market_segments.sort(key=lambda input: input[0])
+
+        # distribute uesrs with the lowest price first
+        reach = campaign.reach
+        total_reach = 0
+        index = 0
+        campaign_allocations = []
+        while total_reach < reach:
+            if index >= len(sub_market_segments): break
+            price, segment = sub_market_segments[index]
+            allocation = min(user_frequencies[segment], reach - total_reach)
+            user_frequencies[segment] -= allocation
+            total_reach += allocation
+            campaign_allocations.append((segment, price, allocation))
+            index += 1
+        allocation_table[campaign] = campaign_allocations
     
-    print(critical_bids)
+    # return allocation table
+    return allocation_table
 
-
-
-
-
-    # # get a list of all the critical bids
-    # count = 0
-    # critical_bids = []
-    # campaign_indices = []
-    # campaigns_list = []
-    # for campaign in campaigns:
-    #     campaigns_list.append(campaign)
-    #     campaign_indices.append(count)
-    #     count += 1
-    #     critical_bids.append(campaign.budget / campaign.reach)
-
-    #     marketsegments = []
-    #     for target_user in user_frequencies:
-    #         if campaign.target_segment.issuperset(target_user):
-    #             marketsegments.append(target_user)
-
-    # print(critical_bids)
-
-    # # allocation and price tables
-    # allocation_price_dict = {}
-
-    # # iterate over highest
-    # critical_bids = np.array(critical_bids)
-    # campaign_indices = np.array(campaign_indices)
-
-    # # sort the campaigns by critical bids
-
-
-
-
-    # while (len(campaign_indices > 0)):
-
-
-    #     arg_max = np.argmax(critical_bids)
-    #     critical_bid = critical_bids[arg_max]
-    #     campaign_index = campaign_indices[arg_max]
-    #     market_segment : MarketSegment = campaigns_list[campaign_index].target_segment
-
-    #     critical_bids = np.delete(critical_bids, arg_max)
-    #     campaign_indices = np.delete(campaign_indices, arg_max)
-
-
-    #     # might as well just sort them all and then iterate through using the stuff...
-    #     # use tuples and stuff instead...
-
-
-    #     # find the second highest in a competing market segment
-    #     campaign_search_critical_bids = critical_bids
-    #     campaign_search_campaign_indices = campaign_indices
-    #     second_price = 0
-    #     while (len(campaign_search_campaign_indices) > 0):
-    #         campaign_search_arg_max = np.argmax(campaign_search_critical_bids)
-    #         campaign_search_campaign_index = campaign_indices[campaign_search_arg_max]
-    #         campain_search_market_segment : MarketSegment = campaigns_list[campaign_search_campaign_index].target_segment
-    #         campaign_search_critical_bid = campaign_search_critical_bids[campaign_search_arg_max]
-    #         campaign_search_critical_bids = np.delete(campaign_search_critical_bids, campaign_search_arg_max)
-    #         campaign_search_campaign_indices = np.delete(campaign_search_campaign_indices, campaign_search_arg_max)
-
-
-    #         # be more precise here...
-    #         # there are cases when stuff is split and prices are off and whatever...
-
-    #         # there are just more edge cases and cost splits possible...
-
-
-    #         # check for overlapping segments
-    #         if not market_segment.issubset(campain_search_market_segment) and not market_segment.issuperset(campain_search_market_segment): continue
-
-    #         # found competing segment
-    #         second_price = campaign_search_critical_bid
-
-    #     # find all subsegments
-    #     sub_market_segments = []
-    #     for segment in user_frequencies:
-    #         if market_segment.issuperset(segment):
-    #             sub_market_segments.append(segment)
-
-    #     # distribute users
-    #     n = np.floor(campaigns_list[campaign_index].reach / len(sub_market_segments))
-    #     allocations = []
-    #     for segment in sub_market_segments:
-    #         min = np.min((user_frequencies[segment], n))
-    #         user_frequencies[segment] -= min
-    #         allocations.append((segment, min))
-    #     allocation_price_dict[campaigns_list[campaign_index].uid] = allocations, second_price
-
-    # print(allocation_price_dict)
 
 
 if __name__ == "__main__":
@@ -154,17 +158,16 @@ if __name__ == "__main__":
     # campaigns.add(campaign_0)
     # campaigns.add(campaign_1)
 
-    # run(campaigns)
-
+    # run(0, campaigns)
 
 
     # personal example
 
-    campaign_0 = Campaign(10, MarketSegment(("Male", "Young", "LowIncome")), 0, 2) # reach, target, start, end
+    campaign_0 = Campaign(10, MarketSegment(("Male",)), 0, 2) # reach, target, start, end
     campaign_0.budget = 10
-    campaign_1 = Campaign(11, MarketSegment(("Female", "LowIncome")), 0, 1) # reach, target, start, end
+    campaign_1 = Campaign(11, MarketSegment(("Male", "LowIncome")), 0, 1) # reach, target, start, end
     campaign_1.budget = 15
-    campaign_2 = Campaign(12, MarketSegment(("Male", "Young")), 0, 3) # reach, target, start, end
+    campaign_2 = Campaign(12, MarketSegment(("Male", "Young", "LowIncome")), 0, 3) # reach, target, start, end
     campaign_2.budget = 5
 
     campaigns = set()
@@ -172,38 +175,4 @@ if __name__ == "__main__":
     campaigns.add(campaign_1)
     campaigns.add(campaign_2)
 
-    run(campaigns)
-
-    # print(market_segment_to_key(MarketSegment(("Male", "Young", "LowIncome"))))
-
-
-
-    # TODO:
-        # debug this a little bit with the added complexity...
-            # maybe even use a random number generator to split if the prices on the things are equal...
-            # incorporate the days into the thingy...
-
-        # figure out how to train the rl better...
-        # get agents that are decently smart to train against...
-
-        # smartest agent without using rl...
-
-        # figure out different possible strategies for this...
-            # 2 max random agents...
-            # 2 critical bidding agents
-            # rest are very smart agents...
-
-        # get the rl to predict campaigns out there...
-            # if this proves to be too difficult of a problem, just run the WF or WE on what we do know and then let rl to shade shit higher or lower...
-
-        
-        # more history information neeeded... figure out if we won the auctions or not...
-        # if we did lose that stuff then inputs given as shtuff...
-
-        # tuning hyperparameters... higher learning rate because we don't have 1million training iterations...
-
-
-
-
-        # what's the difference between active campaigns and my campaigns...
-        # why tf does this not include the first campaign... sort these out by the day to filter stuff out...
+    print(run(0, campaigns))
